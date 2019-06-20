489964e50d3d9100f9d2f5acf0da37575580317c
ECCC-CSSP <charles.leblanc2@canada.ca>
2019-06-20 07:57:34 -0300

    Initial commit

---
+++ CSSPPolSourceGroupingExcelFileRead.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSSPPolSourceGroupingExcelFileRead.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
